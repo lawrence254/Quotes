4e5aa75024fb7b88ff03cb09ff86895882940ea9
Lawrence <lawrenslava1@gmail.com>
2018-04-20 20:34:29 +0300

    Move for to left coloumn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
@@ -18,7 +18,8 @@ import { QuotesFormComponent } from './quotes-form/quotes-form.component';
     QuotesFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Quote} from '../quote'
 @Component({
   selector: 'app-quotes-form',
   templateUrl: './quotes-form.component.html',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesFormComponent implements OnInit {
 
+  newQuote = new Quote("","","",new Date(),0,0);
   constructor() { }
 
   ngOnInit() {
