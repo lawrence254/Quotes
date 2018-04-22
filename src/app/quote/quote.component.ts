5b4de99ad005fa758804d12176d193a42502a198
Lawrence <lawrenslava1@gmail.com>
2018-04-22 20:56:02 +0300

    Create generic quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,10 @@ import {Quote} from '../quote'
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote('the same for you.','Princess Diana','BrainyQuotes.com',new Date(2018,1,12),0,0),
-    new Quote('the same for you.','Princess Diana','BrainyQuotes.com',new Date(2016,1,12),0,0)
+    new Quote('I think being in love with life is a key to eternal youth.','Doug Hutchison','success.com',new Date(2018,1,12),0,0),
+    new Quote('A man who dares to waste one hour of time has not discovered the value of life.','Charles Darwin','Lawrence',new Date(2018,4,12),10,3),
+    new Quote('You’re only here for a short visit. Don’t hurry, don’t worry. And be sure to smell the flowers along the way.',
+    'Walter Hagen','Samuel KNight',new Date(2018,3,12),6,1)
   ]
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
